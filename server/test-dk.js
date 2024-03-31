588c07d9107e8bfd2865faaefcab1f673c23c7fa
MouseLoveRices <148694104+MouseLoveRices@users.noreply.github.com>
2024-03-31 19:48:20 +0700

    Update test-dk.js

--- server/test-dk.js
+++ server/test-dk.js
@@ -1,33 +1,7 @@
-import { signup, signin, googleAuth } from '../server/controllers/auth.js';
+import { googleAuth } from '../server/controllers/auth.js';
 import { assert } from 'chai';
 
 
-
-  describe('signin', () => {
-    it('should signin with correct credentials', async () => {
-      const req = {
-        body: {
-          name: 'test1',
-          password: '123456',
-        },
-      };
-      const res = {
-        status: () => {},
-        json: () => {}
-      };
-      const next = () => {};
-  
-      try {
-        await signin(req, res, next);
-        assert.equal(res.status(), 200);
-        // Kiểm tra các thông tin user trả về ở đây nếu cần
-      } catch (error) {
-        // Xử lý nếu có lỗi xảy ra
-        assert.fail(error);
-      }
-    });
-  });
-  
   describe('googleAuth', () => {
     it('should signin with Google if user exists', async () => {
       // Viết test cho trường hợp đăng nhập bằng Google với user đã tồn tại
